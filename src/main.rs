86f91e9102260fd712de06f53d5caf3893413443
jraregris <oddmund@oddmundo.com>
2016-12-07 22:39:48 +0100

    Find relative

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::fs::File; 
+use std::fs::File;
 use std::io::Read;
 
 fn main() {
@@ -8,20 +8,29 @@ fn main() {
         .next()
         .expect("må ha fil");
 
-    let needle : Vec<_> = "MARIO".bytes().collect();
+    let needle = "MARIO".bytes().collect::<Vec<_>>();
+    let needle = normalize(&needle);
 
     println!("looking for \"{:?}\" in {}", needle, haystack_name);
 
-    let haystack : Vec<_> = File::open(haystack_name)
+    let haystack = File::open(haystack_name)
         .expect("kunke opne fil")
         .bytes()
         .map(|r| r.expect("i/o error eller noe"))
-        .collect();
+        .collect::<Vec<_>>();
 
     for i in 0..haystack.len() - needle.len() {
         let straw = &haystack[i..i+needle.len()];
-        if straw == &needle[..] {
+        let strawn = normalize(straw);
+        if strawn == needle {
             println!("FOUND! {:?} -> {}", straw, String::from_utf8_lossy(straw));
         }
     }
+}
+
+fn normalize(input : &[u8]) -> Vec<i16> {
+    let null = input[0] as i16;
+    input.iter()
+        .map(|i| (*i as i16) - null)
+        .collect()
 }
